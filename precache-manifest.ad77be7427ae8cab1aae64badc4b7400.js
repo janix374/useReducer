060de6cf3f75f5ffe99882017c7742b62ec55011
janix374 <janko.kostic@yahoo.com>
2021-02-17 14:42:38 +0100

    Updates

---
+++ precache-manifest.ad77be7427ae8cab1aae64badc4b7400.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6467d236dcd5f657d2cbf1717577c77",
+    "url": "/useReducer/index.html"
+  },
+  {
+    "revision": "9e928c058bc4a9f6e284",
+    "url": "/useReducer/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "c15a4240da01b6c25bdb",
+    "url": "/useReducer/static/css/main.0f7935fb.chunk.css"
+  },
+  {
+    "revision": "9e928c058bc4a9f6e284",
+    "url": "/useReducer/static/js/2.7a1a34f7.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/useReducer/static/js/2.7a1a34f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c15a4240da01b6c25bdb",
+    "url": "/useReducer/static/js/main.79392ba7.chunk.js"
+  },
+  {
+    "revision": "d274831bec3b9ad28e83",
+    "url": "/useReducer/static/js/runtime-main.906feea9.js"
+  }
+]);
